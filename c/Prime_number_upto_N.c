d52944b5cec4ecfab09e94f835fcc062c038dfbe
vaibhav mishra <89639461+VaibhavM01@users.noreply.github.com>
2021-10-04 17:03:36 +0530

    Prime number printing upto n using c programing
    This program in C is to Find  and print the Prime numbers Upto N.

---
+++ c/Prime_number_upto_N.c
@@ -0,0 +1,25 @@
+//Printing Prime Numbers Upto "N" using c .
+
+
+#include<stdio.h>
+int main()
+{
+    int n,i,fact,j;
+  
+    printf("Enter the Number");
+    scanf("%d",&n);
+  
+    printf("Prime Numbers are: \n");
+    for(i=1; i<=n; i++)
+    {
+        fact=0;
+        for(j=1; j<=n; j++)
+        {
+            if(i%j==0)
+                fact++;
+        }
+        if(fact==2)
+            printf("%d " ,i);
+    }
+    return 0;
+}
